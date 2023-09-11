02f6c35a6867f32891fbd76f6643b8685cc394d3
lelek337 <ioan.af@mail.ru>
2023-09-11 21:36:51 +0300

    my application throws oniqueeanefinitionxception.

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,18 @@
 package app.config;
 
+import app.model.Dog;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+    @Bean(name="dog")
+//    @Scope("prototype")
+    public Dog getDog() {
+        Dog dog = new Dog();
+        return dog;
+    }
 }

dbbecfcbe49b8f96a341075346b8e5489a1cdada
lelek337 <ioan.af@mail.ru>
2023-10-05 17:58:44 +0300

    I'm fixed this task

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,10 +9,23 @@ public class App {
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
 
+        ApplicationContext applicationContext2 =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        System.out.println(bean.equals(bean2));
+
         ApplicationContext applicationContextCat =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         Cat beanCat =
                 (Cat) applicationContextCat.getBean("cat");
         System.out.println(beanCat.getName());
+        ApplicationContext applicationContextCat2 =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        Cat beanCat2 =
+                (Cat) applicationContextCat.getBean("cat");
+        System.out.println(beanCat2.getName());
+        System.out.println(beanCat.equals(beanCat2));
     }
 }

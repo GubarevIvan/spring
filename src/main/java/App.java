3fd1a8c834f704e00bce77d8de30656a5390f73a
lelek337 <ioan.af@mail.ru>
2023-10-07 13:56:47 +0300

    This is my last commit in this task

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,22 +9,19 @@ public class App {
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
 
-        ApplicationContext applicationContext2 =
-                new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean2 =
-                (HelloWorld) applicationContext2.getBean("helloworld");
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean2.getMessage());
         System.out.println(bean.equals(bean2));
 
-        ApplicationContext applicationContextCat =
-                new AnnotationConfigApplicationContext(AppConfig.class);
+
         Cat beanCat =
-                (Cat) applicationContextCat.getBean("cat");
+                (Cat) applicationContext.getBean("cat");
         System.out.println(beanCat.getName());
-        ApplicationContext applicationContextCat2 =
-                new AnnotationConfigApplicationContext(AppConfig.class);
+
         Cat beanCat2 =
-                (Cat) applicationContextCat2.getBean("cat");
+                (Cat) applicationContext.getBean("cat");
         System.out.println(beanCat2.getName());
         System.out.println(beanCat.equals(beanCat2));
     }
